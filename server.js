38b0a2e2fd2bf8f7a27dbb9e75b798c0e0215d80
mgenius3 <benmos16@gmail.com>
2023-03-11 16:34:40 +0100

    fix reg

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+// const { createServer } = require('https');
+// const { parse } = require('url');
+// const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'rierefarm.com.ng';
-const port = process.env.PORT || 3000;
+// const dev = process.env.NODE_ENV !== 'production';
+// const hostname = 'rierefarm.com.ng';
+// const port = process.env.PORT;
 
-const app = next({ dev, hostname, port, dir: '.', conf: { distDir: '.next' } });
-const handle = app.getRequestHandler();
+// const app = next({ dev, hostname, port });
+// const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      // const { pathname, query } = parsedUrl;
-      // if (pathname) {
-      //   console.log(pathname);
-      //   let a = await app.render(req, res, pathname, query);
-      //   console.log('28', a);
-      // } else if (pathname) {
-      //   let b = await app.render(req, res, pathname, query);
-      //   console.log('31', b);
-      // } else {
-      //   await handle(req, res, parsedUrl);
-      // }
-      await handle(req, res, parsedUrl);
-    } catch (err) {
-      res.json({ msg: err });
-    }
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
-  });
-});
+// app.prepare().then(() => {
+//   createServer(async (req, res) => {
+//     try {
+//       const parsedUrl = parse(req.url, true);
+//       // const { pathname, query } = parsedUrl;
+//       // if (pathname) {
+//       //   console.log(pathname);
+//       //   let a = await app.render(req, res, pathname, query);
+//       //   console.log('28', a);
+//       // } else if (pathname) {
+//       //   let b = await app.render(req, res, pathname, query);
+//       //   console.log('31', b);
+//       // } else {
+//       //   await handle(req, res, parsedUrl);
+//       // }
+//       await handle(req, res, parsedUrl);
+//     } catch (err) {
+//       res.json({ msg: err });
+//     }
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://${hostname}:${port}`);
+//   });
+// });
 
 // const { createServer } = require('https');
 // const { parse } = require('url');
@@ -58,3 +58,60 @@ app.prepare().then(() => {
 //     console.log('> Ready on https://localhost:443');
 //   });
 // });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  const server = express();
+
+  // User data for testing purposes
+  const users = [
+    { id: 1, username: 'user1', password: 'pass1' },
+    { id: 2, username: 'user2', password: 'pass2' },
+    { id: 3, username: 'user3', password: 'pass3' },
+  ];
+
+  // Middleware to parse JSON request body
+  server.use(bodyParser.json());
+
+  // Route for checking user exists
+  server.get('/api/users/:username', (req, res) => {
+    const user = users.find((u) => u.username === req.params.username);
+    if (user) {
+      res.status(200).send('User found!');
+    } else {
+      res.status(404).send('User not found!');
+    }
+  });
+
+  // Route for logging in user
+  server.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(
+      (u) => u.username === username && u.password === password
+    );
+    if (user) {
+      res.status(200).send('Login successful!');
+    } else {
+      res.status(401).send('Invalid login credentials!');
+    }
+  });
+
+  // Default route handler
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
