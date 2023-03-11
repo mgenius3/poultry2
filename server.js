6aec45af829b51ab2b9ad7e158cc04955520ea92
mgenius3 <benmos16@gmail.com>
2023-03-11 08:47:36 +0100

    fix login

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
 
+const PORT = process.env.PORT;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev, dir: '.', conf: { distDir: '.next' } });
 const handle = app.getRequestHandler();
@@ -52,7 +53,7 @@ app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(443, (err) => {
+  }).listen(PORT, (err) => {
     if (err) throw err;
     console.log('> Ready on https://localhost:443');
   });
